f54032f7d1ee31b78f3dd09d8f171d9a27f24951
lifeng <444300390@qq.com>
2020-06-12 20:29:59 +0800

    init add

---
+++ demo04.py
@@ -0,0 +1,22 @@
+# price=input("商品单价：")
+# count=input("商品数量：")
+# cost=input("获取金额：")
+# total_cost=float(cost)-float(price)*float(count)
+# print(str(total_cost))
+
+# price=input("分钟：")#20
+# hour=input("小时：")#5
+# day=input("天：")#3
+# second=float(price)*60+float(hour)*60*60+float(day)*24*3600
+# print(str(second))
+
+# liang=float(input("两数："))#20
+# jin=str(liang//16)
+# liang=str(liang%16)
+# print(jin+"斤"+liang)
+distance=float(input("距离："))
+time=float(input("时间："))
+inltial=float(input("初速度"))
+accereated_speed=(distance-inltial*time)*2/(time**2)
+print(str(accereated_speed))
+
---
+++ exercise4.py
@@ -0,0 +1,16 @@
+# 请输入第一个变量
+first=input("第一个变量是")
+# 请输入第二个变脸
+second=input("第二个变量是")
+# 交换
+third=first
+first=second
+second=third
+# 第一个变量是
+print("第一个变量是"+first)
+print("第二个变量是"+second)
+# 第二个变量是
+print("再变回来："+"")
+first,second=second,first
+print("第一个变量是"+first)
+print("第二个变量是"+second)
